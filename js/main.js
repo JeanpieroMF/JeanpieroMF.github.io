5ebab7887b78dab8c8494cbecc424d3f52f755e3
JeanpieroMF <143563489+JeanpieroMF@users.noreply.github.com>
2023-09-30 20:58:46 -0500

    addsnuncios

--- js/main.js
+++ js/main.js
@@ -2,10 +2,25 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
+const salirads1 = document.querySelector(".cerrar-ads1");
+const ads1 = document.querySelector(".ads1");
+const sinads1 = document.querySelector(".cerrar-sinads1");
+
+
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 })
-
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+salirads1.addEventListener("click", () => {
+    ads1.classList.add("hiden");
+}) 
+sinads1.addEventListener("click", () => {
+    ads1.classList.remove("hiden");
+}) 
+
+
+
+
